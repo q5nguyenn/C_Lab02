0c1acc995eb6d550dcd1c791732750cd4bbf30e2
q5nguyenn <q5nguyenn@gmail.com>
2022-06-05 00:20:20 +0700

    Update

--- Bai 02.c
+++ Bai 02.c
@@ -4,8 +4,8 @@
 main(){
 int num = 10; //Kieu du lieu so nguyen
 char ch = 'A'; // Kieu du lieu ky tu
-float f = 25.3; // Kieu du lieu so thuc float co mien rong hon , chinh xac hon float, co the co 10 con so phan thap phan
-double d = 25.3; // Kieu du lieu so thuc double co mien gia tri rat lon so voi float
+float f = 25.3; // Kieu du lieu so thuc float 
+double d = 25.3; // Kieu du lieu so thuc double co mien rong hon , chinh xac hon float, co the co 10 con so phan thap phan
 printf("num = %d\n", num); // in ra man hinh so num, ket qua se la so 10.
 printf("ch = %c\n", ch); // in ra man ky tu A
 printf("f = %.2f\n", f); // in ra man hinh kieu so thuc, 2 con so phan thap phan, ket qua se la 25.30
