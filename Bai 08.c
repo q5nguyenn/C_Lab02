4ffc9e3b01518fd8dfdea810b6facaea53f1545a
q5nguyenn <q5nguyenn@gmail.com>
2022-06-05 12:25:19 +0700

    Update

--- Bai 08.c
+++ Bai 08.c
@@ -4,13 +4,13 @@
 int main(void)
 {
 	printf ("Kieu int: %d Byte \n", sizeof(int));
-	printf ("So nguyen %d \n", 1);
+	printf ("So nguyen: %d \n\n", 15);
 	printf ("Kieu float: %d Byte \n", sizeof(float));
-	printf ("So nguyen %f \n", 3.456);
+	printf ("So thuc kieu float %f \n\n", 3.456);
 	printf ("Kieu double: %d Byte \n", sizeof(double));
-	printf ("So nguyen %lf \n", 3.4567891235);
+	printf ("So thuc kieu double %lf \n\n", 3.4567891235);
 	printf ("Kieu char: %d Byte \n", sizeof(char));
-	printf ("So nguyen %c \n", 'E');
+	printf ("Ky tu %c \n\n", 'E');
 	printf ("Kieu long int: %d Byte \n", sizeof(long int));
 	printf ("Kieu long double: %d Byte \n", sizeof(long double));
 	return 0;
